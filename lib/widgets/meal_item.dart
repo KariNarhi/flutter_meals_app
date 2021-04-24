74b4cab996a61b8076a1f9c2c0ceb243bfb7dc05
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-04-24 17:17:16 +0300

    Pop page and pass data back

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -9,6 +9,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
   MealItem({
     @required this.id,
@@ -17,6 +18,7 @@ class MealItem extends StatelessWidget {
     @required this.duration,
     @required this.affordability,
     @required this.complexity,
+    @required this.removeItem,
   });
 
   String get complexityText {
@@ -52,10 +54,16 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed(
+    Navigator.of(ctx)
+        .pushNamed(
       MealDetailScreen.routeName,
       arguments: id,
-    );
+    )
+        .then((mealId) {
+      if (mealId != null) {
+        removeItem(mealId);
+      }
+    });
   }
 
   @override
